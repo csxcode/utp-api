a842b6ff8bd1ea177d16ff0e2188822044138662
Carlos Silva <csilva@peruapps.com.pe>
2021-07-10 02:00:08 -0500

    add code to apps folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RouterModule } from 'nest-router';
-import { AuthModule } from './admin/auth/auth.module';
-import { AuthModule as AuthModuleWeb } from './web/auth/auth.module';
+import { AuthModule } from './apps/admin/auth/auth.module';
+import { AuthModule as AuthModuleWeb } from './apps/web/auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { routes } from './routes';
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from 'nest-router';
-import { AuthModule } from './admin/auth/auth.module';
-import { AuthModule as AuthModuleWeb } from './web/auth/auth.module';
+import { AuthModule } from './apps/admin/auth/auth.module';
+import { AuthModule as AuthModuleWeb } from './apps/web/auth/auth.module';
 
 export const routes: Routes = [
     {
